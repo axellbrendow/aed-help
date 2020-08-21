2a4acf6d97428c43dac3f3585b59e241eb065b02
axell-brendow <breno.axel@gmail.com>
2020-08-21 12:17:52 -0300

    Add pub.out and BooleanAlgebra in Java

---
+++ tp1/Parte1/BooleanAlgebra/BooleanAlgebra.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class BooleanAlgebra
+{
+    public static String evaluateExpression(String expression)
+    {
+        while (expression.length() > 1)
+        {
+            expression = expression.replace("not(0)", "1");
+            expression = expression.replace("not(1)", "0");
+
+            expression = expression.replace("or(1)", "1");
+            expression = expression.replace("or(0)", "0");
+            expression = expression.replace("or(0,0", "or(0");
+            expression = expression.replace("or(0,1", "or(1");
+            expression = expression.replace("or(1,0", "or(1");
+            expression = expression.replace("or(1,1", "or(1");
+
+            expression = expression.replace("and(1)", "1");
+            expression = expression.replace("and(0)", "0");
+            expression = expression.replace("and(0,0", "and(0");
+            expression = expression.replace("and(0,1", "and(0");
+            expression = expression.replace("and(1,0", "and(0");
+            expression = expression.replace("and(1,1", "and(1");
+        }
+
+        return expression;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner myscan = new Scanner(System.in);
+        String line = myscan.nextLine();
+
+        while (line.charAt(0) != '0')
+        {
+            try (Scanner scan = new Scanner(line)) {
+                int numOperands = scan.nextInt();
+                int[] operands = new int[3];
+    
+                for (int i = 0; i < numOperands; i++)
+                    operands[i] = scan.nextInt();
+    
+                String expression = scan.nextLine()
+                    .replace(" ", "")
+                    .replace("A", "" + operands[0])
+                    .replace("B", "" + operands[1])
+                    .replace("C", "" + operands[2]);
+    
+                System.out.println(evaluateExpression(expression));
+            }
+
+            line = myscan.nextLine();
+        }
+
+        myscan.close();
+    }
+}
